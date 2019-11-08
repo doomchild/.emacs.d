520e25bb0d1c73495d5a2100a07e93b0baeec591
Lee Crabtree <lee.crabtree@gmail.com>
2019-11-08 01:13:24 -0600

    Added org-jira and Catalina permissions

--- elisp/core-org.el
+++ elisp/core-org.el
@@ -2,6 +2,11 @@
 ;;; Org
 
 (use-package org
+  :ensure t
   :config (add-hook 'yas-minor-mode-on org-mode-hook))
+
+(use-package org-jira
+  :ensure t
+  :after org)
 
 (provide 'core-org)
--- init.el
+++ init.el
@@ -6,11 +6,13 @@
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
 
 (require 'core)
+(require 'core-permissions)
 (require 'core-package)
 (require 'core-extensions)
 (require 'core-font)
 (require 'core-theme)
 (require 'core-modeline)
+(require 'core-org)
 (require 'core-ivy)
 (require 'core-editor)
 (require 'core-company)
@@ -25,6 +27,9 @@
 (require 'module-python)
 (require 'module-yaml)
 (require 'module-xml)
+
+(if (file-readable-p (concat user-emacs-directory (file-name-as-directory "private") "private.el"))
+  (load-file (concat user-emacs-directory (file-name-as-directory "private") "private.el")))
 
 (let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
   (message "Loaded configuration in %.3fs" elapsed))
