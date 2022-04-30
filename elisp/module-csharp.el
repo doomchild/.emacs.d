2696f4bc4ba5cc3d74485291eff0cccd5fa421c3
doomchild <lee.crabtree@gmail.com>
2022-04-30 12:20:01 -0500

    Fixed LSP Omnisharp configuration bits

--- elisp/module-csharp.el
+++ elisp/module-csharp.el
@@ -4,12 +4,13 @@
 (use-package csharp-mode
   :demand t
   :mode "\\.cs\\'"
+  :after company
   :config
-  (add-hook 'csharp-mode-hook #'lsp))
+  (add-hook 'csharp-mode-hook #'lsp)
 
-(setq
- lsp-csharp-omnisharp-roslyn-store-path (format "%s/download/omnisharp-roslyn.zip" private-directory)
- lsp-csharp-omnisharp-roslyn-server-dir (format "%s/lsp/omnisharp-roslyn/latest/omnisharp-roslyn" private-directory)
- lsp-csharp-server-install-dir (format "%s/lsp/omnisharp-roslyn" private-directory))
+  (setq
+    lsp-csharp-omnisharp-roslyn-store-path (format "%s/download/omnisharp-roslyn.zip" private-directory)
+    lsp-csharp-omnisharp-roslyn-server-dir (format "%s/lsp/omnisharp-roslyn/latest/omnisharp-roslyn" private-directory)
+    lsp-csharp-server-install-dir (format "%s/lsp/omnisharp-roslyn" private-directory)))
 
 (provide 'module-csharp)
