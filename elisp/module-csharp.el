2bbc6e428d7db9e41eb18fb18bfeff51d71cc83e
doomchild <lee.crabtree@gmail.com>
2022-12-28 16:48:06 -0600

    Fixed Omnisharp paths

--- elisp/module-csharp.el
+++ elisp/module-csharp.el
@@ -14,8 +14,7 @@
   (add-hook 'dc/csharp-mode-hook #'lsp)
 
   (setq
-    lsp-csharp-omnisharp-roslyn-store-path (format "%s/download/omnisharp-roslyn.zip" private-directory)
-    lsp-csharp-omnisharp-roslyn-server-dir (format "%s/lsp/omnisharp-roslyn/latest/omnisharp-roslyn" private-directory)
-    lsp-csharp-server-install-dir (format "%s/lsp/omnisharp-roslyn" private-directory)))
+   lsp-csharp-omnisharp-roslyn-server-dir (format "%s/lsp/omnisharp-roslyn/latest/omnisharp-roslyn" private-directory)
+   lsp-csharp-omnisharp-roslyn-binary-path (format "%s/OmniSharp" lsp-csharp-omnisharp-roslyn-server-dir)))
 
 (provide 'module-csharp)
