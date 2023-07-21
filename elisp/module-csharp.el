f26bd105b352605e4e2591fd1e71e285648cb944
doomchild <lee.crabtree@gmail.com>
2023-07-21 10:10:40 -0500

    Turned off csharp-mode package for versions before emacs 29 (it's included by default now)

--- elisp/module-csharp.el
+++ elisp/module-csharp.el
@@ -1,20 +1,21 @@
 ;;; -*- lexical-binding: t; -*-
 ;;; C#
 
-(defun dc/csharp-mode-hook ()
-  (editorconfig-mode 1)
-  (editorconfig-apply)
-  (lsp))
+(when (version<= emacs-version "29")
+  (defun dc/csharp-mode-hook ()
+    (editorconfig-mode 1)
+    (editorconfig-apply)
+    (lsp))
 
-(use-package csharp-mode
-  :demand t
-  :mode "\\.cs\\'"
-  :after company
-  :config
-  (add-hook 'dc/csharp-mode-hook #'lsp)
+  (use-package csharp-mode
+    :demand t
+    :mode "\\.cs\\'"
+    :after company
+    :config
+    (add-hook 'dc/csharp-mode-hook #'lsp)
 
-  (setq
-   lsp-csharp-omnisharp-roslyn-server-dir (format "%s/lsp/omnisharp-roslyn/latest/omnisharp-roslyn" private-directory)
-   lsp-csharp-omnisharp-roslyn-binary-path (format "%s/OmniSharp" lsp-csharp-omnisharp-roslyn-server-dir)))
+    (setq
+     lsp-csharp-omnisharp-roslyn-server-dir (format "%s/lsp/omnisharp-roslyn/latest/omnisharp-roslyn" private-directory)
+     lsp-csharp-omnisharp-roslyn-binary-path (format "%s/OmniSharp" lsp-csharp-omnisharp-roslyn-server-dir))))
 
 (provide 'module-csharp)
