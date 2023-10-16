32d53316487e4aa976b1dbdbca8db3728832864c
doomchild <lee.crabtree@gmail.com>
2023-10-16 09:21:57 -0500

    Updated font stuff to work for emacsclient frames

--- elisp/core-font.el
+++ elisp/core-font.el
@@ -1,7 +1,15 @@
 ;;; -*- lexical-binding: t; -*-
 ;;; Font Stuff
 
-(when (member "Inconsolata for Powerline" (font-family-list))
-  (set-face-attribute 'default nil :font "Inconsolata for Powerline" :height 160))
+(defun dc/server-font-hook ()
+  (when (member "Inconsolata for Powerline" (font-family-list))
+    (set-face-attribute 'default nil :font "Inconsolata for Powerline" :height 160))
+  (remove-hook 'server-after-make-frame-hook #'dc/server-font-hook))
+
+(add-hook 'server-after-make-frame-hook #'dc/server-font-hook)
+
+(if (not (daemonp))
+    (when (member "Inconsolata for Powerline" (font-family-list))
+      (set-face-attribute 'default nil :font "Inconsolata for Powerline" :height 160)))
 
 (provide 'core-font)
